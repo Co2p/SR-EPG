b1e79bc5030765870d2ca6958ed63dab8323550b
Co2p <go.co2p@gmail.com>
2017-07-31 23:30:44 +0200

    https

--- js/templates.js
+++ js/templates.js
@@ -6,6 +6,6 @@ const channelIconTemplate = Handlebars.compile('<kanalicon title="{{name}}" id="
 
 const channelsURL = 'https://api.sr.se/api/v2/channels?format=json&pagination=false';
 const liveAudioURL = Handlebars.compile('https://sverigesradio.se/topsy/direkt/{{id}}.mp3');
-const episodeURL = Handlebars.compile('http://api.sr.se/api/v2/episodes/get?id={{id}}&format=json');
+const episodeURL = Handlebars.compile('https://api.sr.se/api/v2/episodes/get?id={{id}}&format=json');
 
 const ekotAlbumart = '"https://static-cdn.sr.se/sida/images/4540/3634468_2048_1152.jpg?preset=api-default-square"'
